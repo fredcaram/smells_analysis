26f32acaa0321ba80f97e4065cf718eae99282d0
Frederico <fluiz@avenuecode.com>
2018-01-10 13:54:51 -0200

    Try LightGBM with PUAdapter for Shotgun Surgery

--- models/history_based_model.py
+++ models/history_based_model.py
@@ -65,11 +65,11 @@ class shotgun_surgery_model(history_based_model):
         self.history_based_smells = ["ShotgunSurgery"]#, "ParallelInheritance"
         self.smell_proportion = 0.002
         self.samples_proportion = 0.4
-        self.pu_adapter_enabled = False
+        self.pu_adapter_enabled = True
 
     def get_pipeline(self, smell):
         return Pipeline([("scl", preprocessing.StandardScaler()),
-                            ("ovs",
-                             SMOTETomek(ratio=self.get_ratio, smote=SMOTE(k_neighbors=3, ratio=self.get_ratio),
-                                        tomek=TomekLinks(ratio=self.get_ratio))),
+                            # ("ovs",
+                            #  SMOTETomek(ratio=self.get_ratio, smote=SMOTE(k_neighbors=3, ratio=self.get_ratio),
+                            #             tomek=TomekLinks(ratio=self.get_ratio))),
                             ("clf", self.get_puAdapter(smell))])
